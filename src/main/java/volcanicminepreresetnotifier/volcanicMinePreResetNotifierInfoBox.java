0db4c192a793cf9310803aed4c8b051302213057
magnusrn <magnusn@hotmail.co.uk>
2021-04-13 16:48:48 +0100

    updated runelite ver to fix errors

--- src/main/java/volcanicminepreresetnotifier/volcanicMinePreResetNotifierInfoBox.java
+++ src/main/java/volcanicminepreresetnotifier/volcanicMinePreResetNotifierInfoBox.java
@@ -10,7 +10,7 @@ public class volcanicMinePreResetNotifierInfoBox extends InfoBox {
     private Client client;
 
     public volcanicMinePreResetNotifierInfoBox(Client client, volcanicMinePreResetNotifierPlugin plugin) {
-        super(ImageUtil.getResourceStreamFromClass(volcanicMinePreResetNotifierPlugin.class, "rock.png"), plugin);
+        super(ImageUtil.getResourceStreamFromClass(volcanicMinePreResetNotifierPlugin.class, "/rock.png"), plugin);
         this.plugin = plugin;
         this.client = client;
     }
